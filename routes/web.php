3bd73e872d8b6ae884bba3f93d207e0ffbb276ea
adham37salem <adham.37.salem@gmail.com>
2025-03-17 19:21:53 +0300

    [Feature] Completing designing the view of bills

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Livewire\Front\Bills\ViewOld;
 use App\Livewire\Front\Home;
 use App\Livewire\Front\Login;
 use Illuminate\Support\Facades\Route;
@@ -9,5 +10,6 @@ Route::get('/', Home::class)->name('welcome');
 Route::get("/login", Login::class)->name('login');
 
 Route::group(["prefix" => "bills"], function () {
-    Route::get("old-bills", \App\Livewire\Front\Bills\View::class)->name('bills.old');
+    Route::get("old-bills", ViewOld::class)->name('bills.old');
+    Route::get("view", \App\Livewire\Front\Bills\View::class)->name('bills.view');
 });
