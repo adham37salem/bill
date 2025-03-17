196e12a175940cd53d12c94b70536e7a31a251a5
adham37salem <adham.37.salem@gmail.com>
2025-03-17 19:09:36 +0300

    [Refactor] Adjusting the UI from View to ViewOld

--- app/Livewire/Front/Bills/ViewOld.php
+++ app/Livewire/Front/Bills/ViewOld.php
@@ -5,11 +5,11 @@ namespace App\Livewire\Front\Bills;
 use Livewire\Attributes\Layout;
 use Livewire\Component;
 
-class View extends Component
+class ViewOld extends Component
 {
     #[Layout("components.layouts.app")]
     public function render()
     {
-        return view('livewire.front.bills.view');
+        return view('livewire.front.bills.view-old');
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -9,5 +9,5 @@ Route::get('/', Home::class)->name('welcome');
 Route::get("/login", Login::class)->name('login');
 
 Route::group(["prefix" => "bills"], function () {
-    Route::get("view", \App\Livewire\Front\Bills\View::class)->name('bills.view');
+    Route::get("old-bills", \App\Livewire\Front\Bills\View::class)->name('bills.old');
 });
